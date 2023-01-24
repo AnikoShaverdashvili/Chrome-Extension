90bd3546b9d4bcc9bb8988967158c31b9cb4850e
AnikoShaverdashvili <shaverdashviliania@gmail.com>
2023-01-24 21:17:25 +0400

    Final edit

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const colorPickerBtn = document.querySelector("#color-picker");
 const colorList = document.querySelector(".all-colors");
+const clearAll = document.querySelector(".clear-all");
 const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
 const copyColor = (elem) => {
   navigator.clipboard.writeText(elem.dataset.color);
+  elem.innerText = "Copied";
+  1;
+  setTimeout(() => (elem.innerText = elem.dataset.color), 1000);
 };
+
+//Generate li for the picked color and add to the colorList
 const showColors = () => {
+  if (!pickedColors.length) return;
   colorList.innerHTML = pickedColors
     .map(
       (color) =>
@@ -15,16 +22,17 @@ const showColors = () => {
             <span class="value"data-color=${color}>${color}</span>
           </li>`
     )
-    .join(""); //Generate li for the picked color and add to the colorList
+    .join("");
+
+  document.querySelector(".picked-colors").classList.remove("hide");
+
+  //Add click event listener to copy color code
+  document.querySelectorAll(".color").forEach((li) => {
+    li.addEventListener("click", (e) =>
+      copyColor(e.currentTarget.lastElementChild)
+    );
+  });
 };
-
-//Add a click eventlistener to each color element to copy the color code
-
-document.querySelectorAll(".color").forEach((li) => {
-  li.addEventListener("click", (e) =>
-    copyColor(e.currentTarget.lastElementChild)
-  );
-});
 
 showColors();
 const activateEyeDropper = async () => {
@@ -44,4 +52,12 @@ const activateEyeDropper = async () => {
     console.log(error);
   }
 };
+
+//Clear all colors and update local storage
+const clearAllColors = () => {
+  pickedColors.length = 0;
+  localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+  document.querySelector(".picked-colors").classList.add("hide");
+};
 colorPickerBtn.addEventListener("click", activateEyeDropper);
+clearAll.addEventListener("click", clearAllColors);
